94b18e81a38d191505731daece9917e8acc91eee
thilack <thilackvj54@gmail.com>
2017-06-29 19:37:06 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.t;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("HAI");
+		
+		System.out.println(args[0]);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
